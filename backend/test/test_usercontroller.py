732ea34bb1e3630308397cad8f78a4bf8093cba2
aleighh <aallii.alazawi@gmail.com>
2025-06-03 14:40:20 +0200

    La till kommentarer och tog bort så de e 1 assert

--- backend/test/test_usercontroller.py
+++ backend/test/test_usercontroller.py
@@ -8,6 +8,7 @@ import pytest
 from unittest.mock import Mock
 from src.controllers.usercontroller import UserController
 
+#This test verifies that if you use an invalid user email, it raises a ValueError.
 def test_email_format_invalid():
     dao = Mock()
     ctrl = UserController(dao)
@@ -15,6 +16,7 @@ def test_email_format_invalid():
     with pytest.raises(ValueError):
         ctrl.get_user_by_email("not-an-email")
 
+#This test verifies that if you use an email that doesn't exist, it returns None.
 def test_no_user_found():
     dao = Mock()
     dao.find.return_value = []
@@ -22,6 +24,7 @@ def test_no_user_found():
 
     assert ctrl.get_user_by_email("a@b.com") is None
 
+#This is a normal verification test, testing that if you enter your email correctly.
 def test_single_user_returned():
     dao = Mock()
     expected = {"id": 1, "email": "a@b.com"}
@@ -31,6 +34,8 @@ def test_single_user_returned():
     result = ctrl.get_user_by_email("a@b.com")
     assert result == expected
 
+#This is testing if there are multiple users with the same email in the database both a warning is raised and the first
+# user is returned
 def test_multiple_users(capsys):
     dao = Mock()
     user1 = {"id": 1, "email": "a@b.com"}
@@ -40,9 +45,9 @@ def test_multiple_users(capsys):
 
     result = ctrl.get_user_by_email("a@b.com")
     captured = capsys.readouterr()
-    assert "more than one user found with mail a@b.com" in captured.out
-    assert result == user1
+    assert ("more than one user found with mail a@b.com" in captured.out and result == user1)
 
+#This is testing if the database throws an exception
 def test_dao_exception():
     dao = Mock()
     dao.find.side_effect = Exception("Database error")
