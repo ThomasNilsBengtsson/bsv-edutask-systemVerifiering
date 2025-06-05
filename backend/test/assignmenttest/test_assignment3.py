49eafe1df2041ed130cd05bd9bf51c473d3e1971
aleighh <aallii.alazawi@gmail.com>
2025-06-05 18:22:11 +0200

    final version

--- backend/test/assignmenttest/test_assignment3.py
+++ backend/test/assignmenttest/test_assignment3.py
@@ -18,12 +18,10 @@ def test_db():
 
 def test_dao_insert(test_db):
     dao = DAO("task")
-    insert_data = {"title": "testing", "description": "testing integration", "status": "pending"}
+    insert_data = {"title": "testing", "description": "testing integration"}
     created_doc = dao.create(insert_data)
     id_str = created_doc["_id"]["$oid"]
     found_doc = dao.findOne(id_str)
-    assert found_doc is not None
-    assert found_doc["title"] == insert_data["title"]
     expected_doc_after_find = {"_id": {"$oid": id_str}}
     for key, value in insert_data.items(): # Add all inserted data to expected
         expected_doc_after_find[key] = value    
@@ -36,6 +34,25 @@ def test_dao_insert_invalid(test_db):
     with pytest.raises(pymongo.errors.WriteError): # More specific exception
         dao.create(insert_data)
 
+
+#Testar date med bokstäver
+def test_invalid_date_type(test_db):
+    dao = DAO("task")
+    insert_data = {"title": "testing", "description": "testing integration", "startdate": "blabla", "duedate": "blabla"}
+    with pytest.raises(pymongo.errors.WriteError):
+        dao.create(insert_data)
+
+#testar samma titel, title är uniqueitem
+def test_same_title(test_db):
+    dao = DAO("task")
+    insert_data1 = {"title": "testing", "description": "testing integration1"}
+    dao.create(insert_data1)
+    insert_data2 = {"title": "testing", "description": "testing integration2"}
+    with pytest.raises(pymongo.errors.WriteError):
+        dao.create(insert_data2)
+
+def test_invalid_title(test_db):
+    dao = DAO("task")
     insert_data_wrong_type_title = {"title": 12345, "description": "description here"}
     with pytest.raises(pymongo.errors.WriteError):
         dao.create(insert_data_wrong_type_title)
